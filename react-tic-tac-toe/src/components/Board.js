840366aaf21c4809fe6053f5779eb3c2c0a6f577
vfarah-if <vincent.farah@codurance.com>
2019-05-20 22:54:37 +0100

    Finished right diagonal testing

--- react-tic-tac-toe/src/components/Board.js
+++ react-tic-tac-toe/src/components/Board.js
@@ -63,7 +63,8 @@ class Board extends Component {
             [TOP_MIDDLE, MIDDLE, BOTTOM_MIDDLE],
             [BOTTOM_RIGHT, MIDDLE_RIGHT, TOP_RIGHT],
             // Diagonal Lines
-            [TOP_LEFT, MIDDLE, BOTTOM_RIGHT]
+            [TOP_LEFT, MIDDLE, BOTTOM_RIGHT],
+            [TOP_RIGHT, MIDDLE, BOTTOM_LEFT]
 
         ];
         for (let index = 0; index < lines.length; index++) {
