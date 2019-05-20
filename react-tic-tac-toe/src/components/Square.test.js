d302d5bbc17aec98c1577f5e2649a0c6a241cb5d
vfarah-if <vincent.farah@codurance.com>
2019-05-20 12:19:31 +0100

    Created a square button with onclick event

---
+++ react-tic-tac-toe/src/components/Square.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import Square from './Square';
+
+function getButton(wrapper){
+    return wrapper.find('button.square');
+}
+
+describe('Square', () => {
+    it('should render any value assigned', () => {
+        const wrapper = shallow(<Square value='X'/>)
+        expect(wrapper.text()).toBe('X');
+    });
+
+    it('should render a button that can have a click event assigned', () => {
+        const mockCallBack = jest.fn();
+        const wrapper = shallow(<Square onClick={mockCallBack} />)
+        const buttonElement = getButton(wrapper);
+        
+        expect(buttonElement).toBeTruthy();
+        buttonElement.simulate('click');
+        expect(mockCallBack.mock.calls.length).toEqual(1);
+    });
+
+});
